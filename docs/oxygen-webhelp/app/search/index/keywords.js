dd70953d8e4b7accd33ddb31f0affa7630f55777
“Alison <alison.obright@gmail.com>
2023-10-31 19:42:32 -0500

    First draft commit of oXygen DITA site

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,14 @@
+var keywords=[{w:"Analyzing",p:["p0"]},{w:"data",p:["p0","p3"]},{w:"from",p:["p0"]},{w:"paranormal",p:["p0","p3","p4","p6","p7"]},{w:"investigation",p:["p0","p1","p3","p4","p6","p7"]},{w:"Choosing",p:["p1"]},{w:"site",p:["p1"]},{w:"Conducting",p:["p2"]},{w:"exploratory",p:["p2"]},{w:"interviews",p:["p2"]},{w:"Gathering",p:["p3"]},{w:"during",p:["p3"]},{w:"Method",p:["p4"]},{w:"of",p:["p4","p6"]},{w:"Context",p:["p5"]},{w:"Sensitive",p:["p5"]},{w:"Help",p:["p5"]},{w:"History",p:["p6"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4];
+ph["p1"]=[5, 4, 6];
+ph["p2"]=[7, 8, 9];
+ph["p3"]=[10, 1, 11, 3, 4];
+ph["p4"]=[12, 13, 3, 4];
+ph["p5"]=[14, 15, 16];
+ph["p6"]=[17, 13, 3, 4];
+ph["p7"]=[3, 4];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
